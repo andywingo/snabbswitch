e7bd52d770f281da582870e440fc35fbdea14895
Andy Wingo <wingo@igalia.com>
2018-06-08 09:42:59 +0200

    tmp

--- src/lib/rrd.lua
+++ src/lib/rrd.lua
@@ -4,7 +4,11 @@ module(..., package.seeall)
 
 local ffi = require("ffi")
 local S = require("syscall")
+local lib = require("core.lib")
 local mem = require("lib.stream.mem")
+
+-- Round-robin database.
+local RRD = {}
 
 local rrd_cookie = "RRD\0"
 local rrd_version = "0005\0"
@@ -20,10 +24,8 @@ local fixed_header_t = ffi.typeof [[struct {
    uint64_t unused[10];
 }]]
 
-local data_source_types = { counter=0, absolute=1, gauge=2, derive=3,
-                            cdef=4, dcounter=5, dderive=6 }
--- Only support a subset of consolidation functions.
-local consolidation_functions = { average=0, minimum=1, maximum=2, last=3 }
+local source_types = lib.set('counter', 'gauge')
+local consolidation_functions = lib.set('average', 'minimum', 'maximum', 'last')
 
 -- The bit that's after the header, that depends on the shape of the RRD
 -- file.
@@ -103,7 +105,7 @@ end
 
 local function ptr_to(t) return ffi.typeof('$*', t) end
 
-local function open_rrd(ptr, size, filename)
+function open_rrd(ptr, size, filename)
    local rrd = {ptr=ptr, size=size, filename=filename}
    ptr = ffi.cast('uint8_t*', ptr)
    local function read(t)
@@ -124,10 +126,10 @@ local function open_rrd(ptr, size, filename)
                                        tonumber(rrd.var.archives[i].row_count)))
    end
    assert(size == 0)
-   return rrd
+   return setmetatable(rrd, {__index=RRD})
 end
 
-local function open_rrd_file(filename)
+function open_rrd_file(filename)
    local fd, err = S.open(filename, "rdwr")
    if not fd then
       err = tostring(err or "unknown error")
@@ -186,7 +188,7 @@ local function create_rrd(sources, archives, period)
    end
    for i=0,#archives-1 do
       for j=0,#sources-1 do
-         local cdp = var.cdp_prep[i*#archives + j]
+         local cdp = var.cdp_prep[i*#sources + j]
          cdp.value = 0/0
          cdp.unknown_count =
             (var.last_update.seconds - var.pdp_prep.unknown_count) %
@@ -217,10 +219,169 @@ local function create_rrd(sources, archives, period)
    return open_rrd(ptr, len)
 end
 
+function RRD:last_update()
+   local secs = tonumber(self.var.last_update.seconds)
+   local usecs = tonumber(self.var.last_update.useconds)
+   return secs + usecs * 1e-6
+end
+
+function RRD:isources()
+   local function iter(rrd, i)
+      i = i + 1
+      if i >= rrd.fixed.source_count then return nil end
+      local s = rrd.var.data_sources[i]
+      local name, typ = ffi.string(s.name), ffi.string(s.type)
+      local h, min, max = s.min_heartbeat_period, s.min_value, s.max_value
+      return i, name, typ, tonumber(h), min, max
+   end
+   return iter, self, -1
+end
+
+function RRD:iarchives()
+   local function iter(rrd, i)
+      i = i + 1
+      if i >= rrd.fixed.archive_count then return nil end
+      local a = rrd.var.archives[i]
+      local cf = ffi.string(a.consolidation_function)
+      local rows, window, xff = a.row_count, a.pdp_count, a.min_coverage
+      return i, cf, tonumber(rows), tonumber(window), xff
+   end
+   return iter, self, -1
+end
+
+-- Return all readings at time T, grouped by source name and then by
+-- consolidation function.  If T is positive, it is treated as an
+-- absolute time.  Otherwise it's relative to the last-update time.
+function RRD:ref(t)
+   local last = self:last_update()
+   -- Transform t to be "number of seconds in the past".
+   if t < 0 then t = -t else t = last - t end
+   local ret = {}
+   if t < 0 then return ret end
+   for i, cf, rows, window, xff in self:iarchives() do
+      local interval = window * self.fixed.pdp_step
+      local offset = t / interval
+      if offset < rows then
+         local row = (tonumber(self.var.last_row[i]) - offset) % rows
+         local values = self.archives[i].rows[row].values
+         for j, name, typ, h, min, max in self:isources() do
+            local v = values[j]
+            if ret[name] == nil then ret[name] = {type=typ, cf={}} end
+            if ret[name][cf] == nil then ret[name][cf] = {} end
+            table.insert(ret[name][cf], {interval=interval, value=v})
+         end
+      end
+   end
+   return ret
+end
+
+local function isnan(x) return x~=x end
+
+-- Add a reading, consisting of a map from source names to values.
+function RRD:add(values, t)
+   if t == nil then t = engine.now() end
+   local last = self:last_update()
+   local dt = t - last
+   local step = tonumber(self.fixed.pdp_step)
+   local pre = last % step
+   local steps = math.floor((dt + pre) / step)
+   assert(dt > 0)
+   for i, name, typ, h, lo, hi in self:isources() do
+      local pdp = self.var.pdp_prep[i]
+      local prev = pdp.last_reading.raw
+      local v = values[name]
+      local diff, rate = 0/0, 0/0
+      if v then
+         if typ == 'counter' then
+            if dt < h and prev.is_known then
+               diff = tonumber(v - prev.as_uint64)
+               rate = diff / dt
+            end
+            prev.is_known, prev.as_uint64 = true, v
+         elseif typ = 'gauge' then
+            if dt < h then
+               diff, rate = v * dt, v
+            end
+         else
+            error('unexpected kind', typ)
+         end
+      end
+      if rate < lo or rate > hi then -- Will be false for NaN limits.
+         diff, rate = 0/0, 0/0
+      end
+
+      local tail = step - pre
+      local in_last_pdp = math.min(tail/dt, 1)
+      if isnan(diff) then
+         pdp.unknown_count = pdp.unknown_count + math.min(dt, tail)
+      elseif isnan(pdp.value) then
+         pdp.value = diff * in_last_pdp
+      else
+         pdp.value = pdp.value + diff * in_last_pdp
+      end
+
+         -- Roll over PDP into CDPs.
+      if steps > 0 then
+         local mrhb = self.fixed.min_heartbeat_period
+         local tmp = 0/0
+         if dt < mrhb and pdp.unknown_count < step/2 then
+            tmp = pdp.value / (step - pdp.unknown_count)
+         end
+         -- fixme: commit value!!
+         
+         for j, cf, rows, cdp_step, xff in self:iarchives() do
+            local cdp = self.var.cdp_prep[j*self.fixed.source_count+i]
+            local interval = cdp_step * step
+            local offset = t / interval
+            local archive = self.archives[j]
+            local start_pdp_offset = math.floor((last % interval) / step)
+            local cdp_steps = math.floor((start_pdp_offset + steps) / cdp_step)
+            -- min with row count
+            local steps_in_cdp = math.min(steps, cdp_step - start_pdp_offset)
+            local primary, secondary = 0/0, 0/0
+            if isnan(tmp) then
+               cdp.unknown_count = cdp.unknown_count + steps_in_cdp
+            else
+               -- compute vs initialize??
+               cdp.value = calculate_cdp_value(cdp, tmp, ....)
+               -- fixme for steps of 0 or not 0
+               primary = initialize_cdp_val()
+               secondary = tmp
+            else
+               if isnan(
+               -- update primary and secondary values, write to cdp array
+
+               -- 
+            -- update cdp prep
+        if (update_cdp_prep
+            (rrd, elapsed_pdp_st, start_pdp_offset, rra_step_cnt, rra_idx,
+             pdp_temp, *last_seasonal_coef, *seasonal_coef,
+             current_cf) == -1) {
+            return -1;
+        }
+        end
+
+         -- intervening pdps are unknown
+         local post = t % step
+         if isnan(diff) then
+            pdp.unknown_count = pdp.unknown_count + post
+         else
+            pdp.value = diff * post/dt
+         end
+      end
+         -- update cdp prep areas
+         -- update archives
+      end
+   end
+   -- update last_up
+   return ret
+end
+
 function selftest()
    print('selftest: lib.rrd')
    local empty = create_rrd({}, {}, 1)
    assert(#empty.archives == 0)
+   print(empty:ref(0))
    print('selftest: ok')
 end
 
